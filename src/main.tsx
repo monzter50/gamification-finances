1f74d7ebfb60fdec1a89cb3f55376ba61422e6db
Jose A. Gonzalez <jglez.dev@gmail.com>
2025-05-25 22:15:12 -0500

    refactor: Simplify main entry point by removing router and auth providers
    - Remove BrowserRouter wrapper
    - Remove AuthProvider wrapper
    - Clean up unnecessary imports
    - Simplify component tree structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
 
 import "./index.css";
-import { AuthProvider } from "@/context/AuthContext.tsx";
 
 import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+ 
+    <App />
+   
   </StrictMode>,
 );
